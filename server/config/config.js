98420c332a9dc2c5e034e7f6a37bb017e1226555
manuelguido <manuelguido.gb@gmail.com>
2023-08-29 17:52:55 -0300

    Add Node.js initial backend with Express.js

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+// config/config.js
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  production: {
+    // Production database configuration
+  },
+};
