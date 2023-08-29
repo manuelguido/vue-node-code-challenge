6e30bfd3676918bb4ac232bfd38d294dfd249424
manuelguido <manuelguido.gb@gmail.com>
2023-08-29 20:35:45 -0300

    Update vuejs main.ts configuration

--- client/src/main.ts
+++ client/src/main.ts
@@ -19,18 +19,17 @@ import router from '@/router'
 // Store
 import store from '@/store';
 
-const app = createApp(App)
-
-registerPlugins(app)
-
-app.mount('#app')
-
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const app = createApp(App)
+
+registerPlugins(app)
+
+// Configure vuetify
 const vuetify = createVuetify({
   components,
   directives,
@@ -41,44 +40,3 @@ createApp(App)
   .use(store)
   .use(vuetify)
   .mount('#app')
-
-
-
-
-
-
-
-
-
-
-// import store from './store';
-
-// // Vuetify setup
-// import "vuetify/styles";
-// import { createVuetify } from "vuetify";
-// import * as components from "vuetify/components";
-// import * as directives from "vuetify/directives";
-
-// import { fa } from "vuetify/iconsets/fa";
-// import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
-// import "@mdi/font/css/materialdesignicons.css";
-// import "@fortawesome/fontawesome-free/css/all.css";
-
-// const vuetify = createVuetify({
-//   theme: {
-//     defaultTheme: "light",
-//   },
-//   icons: {
-//     defaultSet: "mdi",
-//     aliases,
-//     sets: {
-//       mdi,
-//       fa,
-//     },
-//   },
-//   components,
-//   directives,
-// });
-
-// // Vue Application setup
-// createApp(app).use(router).use(store).use(vuetify).mount("#app")
